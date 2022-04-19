f11d8c9f118928f4b4ab75c05277912c300889d5
Jérémy Levilain <jeremy@jeremylvln.fr>
2022-04-19 16:59:09 +0200

    feat(fp): use override syntax

--- src/fp.js
+++ src/fp.js
@@ -1,14 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
   plugins: ['fp'],
   extends: ['plugin:fp/recommended'],
   overrides: [
     {
-      files: ['features/**/*', '*InMem.ts', 'test/*', '*.spec.ts'],
+      files: ['features/**/*', '*InMem.ts', 'test/*', '*.*spec.ts'],
       rules: {
         'fp/no-class': 0,
         'fp/no-this': 0,
@@ -19,23 +14,6 @@ module.exports = {
         'fp/no-mutating-methods': 0,
         'fp/no-throw': 0,
       },
-    },
+    }
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: 'tsconfig.json',
-      },
-    },
-  },
 };
